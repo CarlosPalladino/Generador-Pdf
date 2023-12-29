2e7c707570a23a2f2c57fb03945076fa34376c57
Carlo Palladino”git config --global user.name CarlosPalladino” <carlosezequiel19palladino@gmail.com>
2023-12-29 10:24:07 -0300

    Agregar archivos de proyecto.

---
+++ GeneradorDePdfs/Pagina.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace GeneradorDePdfs
+{
+    partial class Principal
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dataGridView1 = new DataGridView();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(213, 57);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(440, 298);
+            dataGridView1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(12, 57);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(178, 298);
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // Principal
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(685, 450);
+            Controls.Add(pictureBox1);
+            Controls.Add(dataGridView1);
+            Name = "Principal";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private DataGridView dataGridView1;
+        private PictureBox pictureBox1;
+    }
+}
---
+++ GeneradorDePdfs/Pagina.cs
@@ -0,0 +1,15 @@
+namespace GeneradorDePdfs
+{
+    public partial class Principal : Form
+    {
+        public Principal()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
